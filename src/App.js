b279f2656ca5abd92e427085f9b6cc331d48e765
sudarsancse <sudarsansarkarcst@gmail.com>
2025-05-20 11:36:18 +0530

    new name

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className=" flex justify-center items-center">
       <h1 className=" text-red-500 font-bold">sudarsan</h1>
+      <h1 className=" text-red-500 font-bold">chandan</h1>
     </div>
   );
 }
